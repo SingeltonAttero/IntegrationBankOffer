f410c0e5f3b38d6a887228479947591b69c94e06
YWeber <yakowweberjavajob@gmail.com>
2023-07-31 18:30:37 +0200

    add text description in internal app

--- app/src/main/java/com/yakow/integrationbankoffer/MainActivity.kt
+++ app/src/main/java/com/yakow/integrationbankoffer/MainActivity.kt
@@ -2,6 +2,13 @@ package com.yakow.integrationbankoffer
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 import com.yakow.moduleintegration.BankIntegration
 
 class MainActivity : ComponentActivity() {
@@ -9,6 +16,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         BankIntegration.integrator.logService().testA()
         BankIntegration.integrator.metricsService().metrics()
+        setContent {
+            Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+                Text(text = "Изначальное приложение аля МИ", fontSize = 24.sp)
+            }
+        }
 
     }
 }
